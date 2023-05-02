c90775f1de9ad73d3cee78bad25a86355b7f4c2e
nonyenathal <nonyeachus@outlook.com>
2023-05-02 12:14:32 -0600

    new commit

---
+++ src/Pages/AboutUs/AboutUsPage.jsx
@@ -0,0 +1,16 @@
+// import Title from "../../../src/Components/Common/Title";
+// import Yen from "../../Assests/YEN.png";
+// import {
+//   CalendarIcon,
+//   LocationMarkerIcon,
+//   ClockIcon,
+// } from "@heroicons/react/solid";
+
+
+function AboutUsPage() {
+    return (
+        <div>This About US Page</div>
+    )
+}
+
+export default AboutUsPage;
--- src/Pages/AboutUs/index.jsx
+++ src/Pages/AboutUs/index.jsx
@@ -1,10 +1,13 @@
 import { Layout } from "../../Components";
+// 1
+import { Events } from "../../Components/Landing/Events"
 
 function AboutUs() {
   return (
     <Layout>
       <div className="mb-[6rem]">
         <h2>About Us</h2>
+        <Events></Events>
       </div>
     </Layout>
   );
