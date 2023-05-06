d0a0cd35ba10889868654252b09e4bc9ac13203a
nonyenathal <nonyeachus@outlook.com>
2023-05-06 12:47:21 -0600

    aboutUs update

--- src/Pages/AboutUs/SectThree.jsx
+++ src/Pages/AboutUs/SectThree.jsx
@@ -7,7 +7,7 @@ import About4 from '../../Assests/about4.png';
 function SectThree() {
     return (
 
-        <div className="w-full bg-lime-200 py-[6rem] px-4">
+        <div className="w-full bg-[#bbf7d0] py-[7rem] px-4">
         <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
             <div className='flex flex-col justify-center p-4'>
             <img className='w-20' src={About3} alt="/" />
@@ -17,7 +17,7 @@ function SectThree() {
                 eligendi natus quasi sit saepe aspernatur rerum quae, commodi, voluptate quos 
                 nobis quia consequuntur amet minima accusamus numquam, pariatur suscipit.
             </p> <br />
-            <p className='font-medium py-1'>
+            <p className='font-medium'>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam corrupti 
                 eligendi natus quasi sit saepe aspernatur rerum quae, commodi, voluptate quos 
                 nobis quia consequuntur amet minima accusamus numquam, pariatur suscipit.
@@ -32,7 +32,7 @@ function SectThree() {
                 eligendi natus quasi sit saepe aspernatur rerum quae, commodi, voluptate quos 
                 nobis quia consequuntur amet minima accusamus numquam, pariatur suscipit.
             </p> <br />
-            <p className='font-medium py-1'>
+            <p className='font-medium'>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam corrupti 
                 eligendi natus quasi sit saepe aspernatur rerum quae, commodi, voluptate quos 
                 nobis quia consequuntur amet minima accusamus numquam, pariatur suscipit.
